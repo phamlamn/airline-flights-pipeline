319478cff5aeffb172445f89bc08d5d4cd9c03dc
Lam Pham <phamlamn@gmail.com>
2024-12-20 13:04:07 -0500

    added creation of and writing to iceberg tables for all sources

---
+++ src/DDL.py
@@ -0,0 +1,73 @@
+import os
+
+CATALOG_NAME = os.environ['CATALOG_NAME']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+
+flights_ddl = f"""
+CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{DATABASE_NAME}.flights (
+    year INT,
+    month INT,
+    day INT,
+    day_of_week INT,
+    airline STRING,
+    flight_number INT,
+    tail_number STRING,
+    origin_airport STRING,
+    destination_airport STRING,
+    scheduled_departure INT,
+    departure_time INT,
+    departure_delay INT,
+    taxi_out INT,
+    wheels_off INT,
+    scheduled_time INT,
+    elapsed_time INT,
+    air_time INT,
+    distance INT,
+    wheels_on INT,
+    taxi_in INT,
+    scheduled_arrival INT,
+    arrival_time INT,
+    arrival_delay INT,
+    diverted INT,
+    cancelled INT,
+    cancellation_reason STRING,
+    air_system_delay INT,
+    security_delay INT,
+    airline_delay INT,
+    late_aircraft_delay INT,
+    weather_delay INT
+)
+USING iceberg
+PARTITIONED BY (month)"""
+
+
+airlines_ddl = f"""
+CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{DATABASE_NAME}.airlines (
+    IATA_CODE STRING,
+    AIRLINE STRING
+)
+USING iceberg
+"""
+
+
+airports_ddl = f"""
+CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{DATABASE_NAME}.airports (
+    IATA_CODE STRING,
+    AIRPORT STRING,
+    CITY STRING,
+    STATE STRING,
+    COUNTRY STRING,
+    LATITUDE FLOAT,
+    LONGITUDE FLOAT
+)
+USING iceberg
+"""
+
+
+cancellation_codes_ddl = f"""
+CREATE TABLE IF NOT EXISTS {CATALOG_NAME}.{DATABASE_NAME}.cancellation_codes (
+    CANCELLATION_REASON STRING,
+    CANCELLATION_DESCRIPTION STRING
+)
+USING iceberg
+"""
--- src/jobs/ingest.py
+++ src/jobs/ingest.py
@@ -1,4 +1,57 @@
+import schemas
+import DDL
+
+import os
+
+CATALOG_NAME = os.environ['CATALOG_NAME']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+
+# Define .csv filepaths
+data_path = '/home/iceberg/data'
+flights_filename = f'{data_path}/flights.csv'
+airlines_filename = f'{data_path}/airlines.csv'
+airports_filename = f'{data_path}/airports.csv'
+cancellation_codes_filename = f'{data_path}/cancellation_codes.csv'
 
 # Read .csv files
+flights_df = spark.read \
+    .schema(schemas.flights_schema) \
+    .option('header', True) \
+    .csv(flights_filename)
+    
+airlines_df = spark.read \
+    .schema(schemas.airlines_schema) \
+    .option('header', True) \
+    .csv(airlines_filename)
 
-# Write to Bronze (Raw) Iceberg tables
+airports_df = spark.read \
+    .schema(schemas.airports_schema) \
+    .option('header', True) \
+    .csv(airports_filename)
+
+cancellation_codes_df = spark.read \
+    .schema(schemas.cancellation_codes_schema) \
+    .option('header', True) \
+    .csv(cancellation_codes_filename)
+
+
+# Create Iceberg tables
+spark.sql(DDL.flights_ddl)
+spark.sql(DDL.airlines_ddl)
+spark.sql(DDL.airports_ddl)
+spark.sql(DDL.cancellation_codes_ddl)
+
+
+# Write to Iceberg Tables
+flights_df.writeTo(f'{CATALOG_NAME}.{DATABASE_NAME}.flights') \
+    .append()
+
+airlines_df.writeTo(f'{CATALOG_NAME}.{DATABASE_NAME}.airlines') \
+    .append()
+
+airports_df.writeTo(f'{CATALOG_NAME}.{DATABASE_NAME}.airports') \
+    .append()
+
+cancellation_codes_df.writeTo(f'{CATALOG_NAME}.{DATABASE_NAME}.cancellation_codes') \
+    .append()
+
